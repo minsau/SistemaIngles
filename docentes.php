ec5be75d6e1ce2cad26cb1989b723576ecf2cc77
AlanGlez <alangonzalez.941025@gmail.com>
2016-01-10 15:06:15 -0600

    Solo Avance

--- docentes.php
+++ docentes.php
@@ -1,3 +1,11 @@
+<?php
+
+require_once("includes/conexion.php");
+$sql = "select m.cedula,p.nombre,p.correo from Persona as p, Maestro as m where p.id_persona=m.id_maestro";
+$res = mysql_query($sql, $con) or die("Hubo un error al obtener los datos: ".mysql_error());
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,7 +34,7 @@
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
-          <span class="icon-bar"></span>                        
+          <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#">Docencia</a>
     </div>
@@ -40,7 +48,7 @@
       </div>
     </div>
   </div>
-</nav>    
+</nav>
 
 <div id="Docentes" class="container-fluid">
   <h1>Docentes </h1>
@@ -56,43 +64,96 @@
       </tr>
     </thead>
     <tbody>
+	<?php
+		while($reg = mysql_fetch_array($res)){
+	?>
       <tr>
-        <td>1</td>
-        <td>Anna</td>
-	<td>anna@gmail.com</td>
+	<td> <?php echo $reg["cedula"];?> </td>
+        <td> <?php echo $reg["nombre"];?> </td>
+	<td> <?php echo $reg["correo"];?> </td>
 	<td>
 		<button type="button" class="btn btn-default btn-sm">
+		<a href="#NuevoDocente">
         	<span class="glyphicon glyphicon-pencil"></span> Editar
+		</a>
 	        </button>
 		<button type="button" class="btn btn-default btn-sm">
    	        <span class="glyphicon glyphicon-remove"></span> Eliminar
+
 	        </button>
 	</td>
       </tr>
-      <tr>
-        <td>2</td>
-        <td>Debbie</td>
-	<td>debbie@gmail.com</td>
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>John</td>
-	<td>john@gmail.com</td>
-      </tr>
+	<?php } ?>
     </tbody>
   </table> </div>
 
 </div> <div id="AsignarGrupos" class="container-fluid">
-  <h1>Asignar Grupos</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation 
-bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation 
-bar while scrolling!</p> </div> <div id="NuevoDocente" class="container-fluid">
-  <h1>Nuevo Docente</h1>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation 
-bar while scrolling!</p>
-  <p>Try to scroll this section and look at the navigation bar while scrolling! Try to scroll this section and look at the navigation 
-bar while scrolling!</p> </div>
 
-</body> </html>
+  <h1>Asignar Grupos</h1>
+ <div class="container">
+  <h2>Form control: inline radio buttons</h2>
+  <p>The form below contains three inline radio buttons:</p>
+  <form role="form">
+    <label class="radio-inline">
+      <input type="radio" name="optradio">Nivel A
+    </label>
+    <label class="radio-inline">
+      <input type="radio" name="optradio">Nivel B
+    </label>
+    <label class="radio-inline">
+      <input type="radio" name="optradio">Nivel C
+    </label>
+  </form>
+ </div>
+</div>
+
+<div id="NuevoDocente" class="container-fluid">
+  <h1>Nuevo Docente</h1>
+<p>Si ustde requiere agregar a un nuevo docente, llene los campos y Guarde.</p>
+ <div class="container">
+  <form class="form-horizontal" role="form">
+    <div class="form-group">
+      <label class="control-label col-sm-2">Nombre:</label>
+      <div class="col-sm-10">
+        <input type="text" name="nombre_decente"class="form-control" id="nombre_docente" placeholder="Digite Nombre">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2">Apellidos:</label>
+      <div class="col-sm-10">
+        <input type="text" name="apellidos_docente" class="form-control" id="apellidos_docente" placeholder="Digite Apellidos">
+      </div>
+    </div>
+
+   <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email:</label>
+      <div class="col-sm-10">
+        <input type="email" name="correo_docente" class="form-control" id="email" placeholder="Digite Correo">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password:</label>
+      <div class="col-sm-10">
+        <input type="password" name="password"  class="form-control" id="pwd" placeholder="Digite Password">
+      </div>
+    </div>
+
+    <div class="form-group">
+      <label class="control-label col-sm-2">Cedula:</label>
+      <div class="col-sm-10">
+        <input type="number" name="cedula" class="form-control" id="cedula" placeholder="Digite Cedula" min="1" max="10">
+      </div>
+    </div>
+
+
+    <div class="form-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-primary">Guardar</button>
+      </div>
+    </div>
+  </form>
+</div>
+</div>
+</body>
+</html>
 
